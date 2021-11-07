2f79d1a1dadc0eb47eb7e84a7e6a3f3ff767e348
vmlysenkov <lysenkov2@ya.ru>
2021-11-07 12:11:07 +0300

    testng created

--- src/main/java/ru/netology/unit/CashbackHacker.java
+++ src/main/java/ru/netology/unit/CashbackHacker.java
@@ -1,7 +1,9 @@
 package ru.netology.unit;
 
 public class CashbackHacker {
-    public static void main(String[] args) {
-        System.out.println("Anya mysh!!!");
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
     }
 }
