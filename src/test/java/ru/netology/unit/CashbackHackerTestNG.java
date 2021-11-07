a1d21f6859510357e7b6946397ca49c76054deee
vmlysenkov <lysenkov2@ya.ru>
2021-11-07 12:16:05 +0300

    test added in testNG

---
+++ src/test/java/ru/netology/unit/CashbackHackerTestNG.java
@@ -0,0 +1,19 @@
+package ru.netology.unit;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackerTestNG {
+
+    @Test
+    public void shouldCalculateRemainTo1000() {
+        CashbackHacker service = new CashbackHacker();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+}
